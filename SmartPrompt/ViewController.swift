a43bd30f5b65de536c688d51f0e8567e5ac988ec
Jason wang <qwang216@gmail.com>
2018-01-08 17:40:14 -0500

    inital commit

---
+++ SmartPrompt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartPrompt
+//
+//  Created by Jason wang on 1/8/18.
+//  Copyright © 2018 Jason wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
